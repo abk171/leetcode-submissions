10aa770fd7eacc7152f328a65bbc7dccca390cea
ak1909552 <82260503+ak1909552@users.noreply.github.com>
2022-05-04 14:21:47 +0300

    Time: 61 ms (28.05%), Space: 83.5 MB (29.77%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,27 +1,16 @@
 class Solution {
-    public int maxOperations(int nums[], int k) {
-        int count = 0;
+    public int maxOperations(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        HashSet<Integer> visited = new HashSet<>();
-        
+        int count = 0;
         for(int i = 0; i < nums.length; i++) {
-            if(map.containsKey(nums[i])) map.put(nums[i], map.get(nums[i]) + 1);
-            else map.put(nums[i], 1);
-        }
-        
-        // for(Integer key : map.keySet()) {
-        //     System.out.printf("%d : %d\n", key, map.get(key));
-        // }
-        
-        for(Integer key : map.keySet()) {
-            if(visited.contains(key)) continue;
+            if(map.containsKey(k - nums[i]) && map.get(k - nums[i]) > 0) {
+                count++;
+                map.put(k - nums[i], map.get(k - nums[i]) - 1);
+            }
             else {
-                if(k % 2 == 0 && key == k/2) count += map.get(key)/2;
-                else if(map.containsKey(k - key)) count += Math.min(map.get(key), map.get(k - key));
-                visited.add(k - key);
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             }
         }
-        
         return count;
     }
 }
