24951f31832d59826e9b802e0ac5b263df693388
Abhigyan Kishor <82260503+ak1909552@users.noreply.github.com>
2022-05-11 18:05:32 +0300

    Time: 57 ms (16.13%), Space: 41.4 MB (19.45%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,21 +1,20 @@
 class Solution {
+    int count = 0;
     public int countVowelStrings(int n) {
-        char[] arr = {'a', 'e', 'i', 'o', 'u'};
-        List<String> result = new ArrayList<>();
-        helper(result, new StringBuilder(), n, 0, arr);
-        // System.out.println(result.toString());
-        return result.size();
+        
+        // char[] arr = {'a', 'e', 'i', 'o', 'u'};
+        helper(n, 0);
+        return count;
     }
     
-    public void helper(List<String> result, StringBuilder curr, int n, int index, char[] arr) {
+    public void helper(int n, int index) {
+        // System.out.println(count);
         if(n <= 0) {
-            result.add(curr.toString());
-            return;
+            count++; return;
         }
-        for(; index < arr.length; index++) {
-            curr.append(arr[index]);
-            helper(result,curr,n-1,index, arr);
-            curr.deleteCharAt(curr.length() - 1);
+        
+        for(;index < 5; index++) {
+            helper( n - 1, index);
         }
     }
 }
