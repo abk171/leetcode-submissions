2aa8590b01ea0f65c84939490e449506abdc5fdd
Abhigyan Kishor <82260503+ak1909552@users.noreply.github.com>
2022-05-29 20:08:16 +0300

    Time: 6 ms (99.26%), Space: 42.4 MB (88.77%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProduct(String[] words) {
+        int[] masks = new int[words.length];
+        for(int i = 0; i < words.length; i++)
+            for(int j = 0; j < words[i].length(); j++)
+                masks[i] |= 1 << (words[i].charAt(j) - 'a');
+        int max = 0;
+        int b_and = 0;
+        int product = 0;
+        for(int i = 0; i < words.length; i++) {
+            for(int j = i + 1; j < words.length; j++) {
+                b_and = masks[i] & masks[j];
+                if(b_and == 0) {
+                    product = words[i].length() * words[j].length();
+                    max = max > product ? max : product;
+                }
+            }
+        }
+        return max;
+       
+    }
+}
