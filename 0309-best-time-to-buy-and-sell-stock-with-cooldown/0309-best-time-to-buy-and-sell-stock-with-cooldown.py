941d58c2cd5fd016ff878ddd0230e9305e27cb4c
Abhigyan Kishor <82260503+ak1909552@users.noreply.github.com>
2022-12-24 00:44:59 +0300

    Time: 32 ms (99.72%), Space: 14.2 MB (81.40%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,19 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         size = len(prices)
-        s0 = [0 for _ in range(size)]
-        s1 = [0 for _ in range(size)]
-        s2 = [0 for _ in range(size)]
-        
-        s0[0] = 0
-        s1[0] = -prices[0]
-        s2[0] = -1001
+        s = [0, -prices[0], -1001]
         
         for i in range(1, size):
-            s0[i] = max(s0[i - 1],s2[i - 1])
-            s1[i] = max(s1[i - 1],s0[i - 1] - prices[i])
-            s2[i] = s1[i - 1] + prices[i]
+            s0 = max(s[0], s[2])
+            s1 = max(s[1], s[0] - prices[i])
+            s2 = s[1] + prices[i]
             
-        return max(s0[-1], s2[-1])
-        
-        
+            s = [s0, s1, s2]
+            
+        return max(s[0], s[2])
