669e5fc164ad34e08f2f7a75f0d44639f8c59c7c
Abhigyan Kishor <82260503+ak1909552@users.noreply.github.com>
2023-01-04 06:57:54 -0800

    Time: 1029 ms (74.26%), Space: 28 MB (96.23%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -12,20 +12,19 @@ class Solution:
         
         for i in m.values():
 
+            if i == 1:
+                return -1
+            
             k = i // 3
             r = i % 3
             
-            if k == 0 and r == 1:
-                return -1
-            elif k == 0 and r == 2:
-                result += 1
+            
+            if r == 0:
+                result += k
+            elif r == 1:
+                result += (k - 1 + 2)
             else:
-                if r == 0:
-                    result += k
-                elif r == 1:
-                    result += (k - 1 + 2)
-                else:
-                    result += (k + 1)
+                result += (k + 1)
                     
         return result
             
