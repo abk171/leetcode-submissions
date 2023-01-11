1d75fdaaa6d7de3165b5c3ebfbbf30e6e134ae9f
Abhigyan Kishor <82260503+ak1909552@users.noreply.github.com>
2023-01-11 14:40:01 -0800

    Time: 1820 ms (14.65%), Space: 65.6 MB (10.19%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -0,0 +1,47 @@
+import queue
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        adj = [[] for _ in range(n)]
+        for edge in edges:
+            adj[edge[0]].append(edge[1])
+            adj[edge[1]].append(edge[0])
+        
+        apples = []
+        for i,apple in enumerate(hasApple):
+            if apple:
+                apples.append(i)
+        
+        distances = [float('inf') for _ in range(n)]
+        distances[0] = 0
+        parent = [i for i in range(n)]
+        
+        q = queue.PriorityQueue()
+        for i, distance in enumerate(distances):
+            q.put((distance, i))
+        
+        visited = [False for _ in range(n)]
+        
+        while not q.empty():
+            distance, x = q.get()
+            for y in adj[x]:
+                if distances[y] > 1 + distance:
+                    distances[y] = 1 + distance
+                    parent[y] = x
+                    q.put((distances[y], y))
+            visited[x] = True
+        
+        visited_edges = set()
+        
+        def find(i):
+            while i != 0:
+                visited_edges.add((i, parent[i]))
+                visited_edges.add((parent[i], i))
+                i = parent[i]
+        
+        for apple in apples:
+            find(apple)
+        
+        
+        
+        return len(visited_edges)
+                    
