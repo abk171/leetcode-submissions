c133909a593ee96ee9bc480430905d50c26a44c7
Abhigyan Kishor <82260503+ak1909552@users.noreply.github.com>
2023-03-12 15:09:47 +0300

    Time: 181 ms (24.11%), Space: 20 MB (5.40%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        def mergeTwo(a, b):
+            res = ListNode(0)
+            rptr = res
+            while a or b:
+                
+                if not a:
+                    rptr.next = ListNode(b.val)
+                    b = b.next
+                    
+                elif not b:
+                    rptr.next = ListNode(a.val)
+                    a = a.next
+                
+                else:
+                    if a.val < b.val:
+                        rptr.next = ListNode(a.val)
+                        a = a.next
+                    else:
+                        rptr.next = ListNode(b.val)
+                        b = b.next
+                rptr = rptr.next
+            return res.next
+        
+        def divide(ptrs):
+            lp = len(ptrs)
+            if lp == 2:
+                return mergeTwo(ptrs[0], ptrs[1])
+            elif lp == 1:
+                return ptrs[0]
+            elif lp == 0:
+                return None
+            
+            left = ptrs[0 : lp // 2]
+            right = ptrs[lp // 2 : ]
+            
+            return mergeTwo(divide(left), divide(right))
+
+        return divide(lists)
+        # return mergeTwo(lists[0], lists[1])
+
+        # return ListNode(0)
+            
+                    
+                    
+            
