6bc4110b7c79f87d4ec93f417792088a26668067
Abhigyan Kishor <82260503+abk171@users.noreply.github.com>
2025-04-20 09:57:23 -0400

    Time: 366 ms (51.88%) | Memory: 18 MB (96.93%) - LeetSync

--- 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.py
+++ 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.py
@@ -1,50 +1,25 @@
 class Solution:
     def numberOfWays(self, s: str) -> int:
-        n = len(s)
-        
-        left = [0 for _ in range(n)]
-        right = [0 for _ in range(n)]
+        total_zero = 0
+        total_one = 0
 
-        # 101
-        count = 0
-        for i, c in enumerate(s):
+        for c in s:
             if c == '0':
-                left[i] = count
+                total_zero += 1
             else:
-                count += 1
+                total_one += 1
         
-        count = 0
-        for i in range(n - 1, -1, -1):
-            c = s[i]
-            if c == '0':
-                right[i] = count
-            else:
-                count += 1
-        
-        m1 = 0
-        for i in range(n):
-            m1 += left[i] * right[i]
-        
-        # 010
-        count = 0
-        for i, c in enumerate(s):
-            left[i] = 0
-            if c == '1':
-                left[i] = count
-            else:
-                count += 1
-        
-        count = 0
-        for i in range(n - 1, -1, -1):
-            c = s[i]
-            right[i] = 0
-            if c == '1':
-                right[i] = count
-            else:
-                count += 1
-        
-        m2 = 0
-        for i in range(n):
-            m2 += left[i] * right[i]
+        left_zero = 0
+        left_one = 0
 
-        return m1 + m2
+        result = 0
+        for c in s:
+            if c == '0':
+                total_zero -= 1 
+                left_zero += 1
+                result += left_one * total_one
+            else:
+                total_one -= 1
+                left_one += 1
+                result += left_zero * total_zero
+        return result
